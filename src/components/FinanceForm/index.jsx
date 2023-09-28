069db5a9b30950e31881c4a72e528388ad2fe6c1
JokerMSD <igorpn2004@gmail.com>
2023-09-28 15:50:49 -0300

    FIX: Modified the initial structure

---
+++ src/components/FinanceForm/index.jsx
@@ -0,0 +1,11 @@
+
+
+export const FinanceForm = () => {
+
+    return (
+       <>
+
+      </>
+  )
+
+}
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+
+
+export const Header = () => {
+
+    return (
+       <>
+        <header>
+           <h1>Joker Movies</h1>
+        </header>
+      </>
+  )
+
+}
