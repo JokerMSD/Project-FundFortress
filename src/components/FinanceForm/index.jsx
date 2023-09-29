4234d226f20be9885196c6b767c2bd988b9c3bc1
JokerMSD <igorpn2004@gmail.com>
2023-09-28 22:46:44 -0300

    FEAT: Modify FinanceForm

--- src/components/FinanceForm/index.jsx
+++ src/components/FinanceForm/index.jsx
@@ -1,23 +1,18 @@
 import Style from "./style.module.css";
+import { useState } from "react";
 
 export const FinanceForm = () => {
   return (
     <>
       <section className={Style.container}>
-        <form action="">
+        <form>
           <label htmlFor="description">Descrição</label>
-          <input
-            name="description"
-            id="description"
-            placeholder="Digite aqui sua descrição"
-          />
+          <input type="text" name="description" id="description" placeholder="Digite aqui sua descrição"/>
 
           <label htmlFor="value">Valor(R$)</label>
-
-          <input name="value" id="value" placeholder="1" />
+          <input type="text" name="value" id="value" placeholder="1" />
 
           <label htmlFor="valueType">Tipo de Valor</label>
-
           <select name="valueType" id="valueType">
             <option value="Entrada">Entrada</option>
             <option value="Saída">Saída</option>
