76ee844e1d7b6c30ecf8b2c0493060701b841e9b
JokerMSD <igorpn2004@gmail.com>
2023-09-30 18:36:16 -0300

    update src/components/FinanceCard/index.jsx

--- src/components/FinanceCard/index.jsx
+++ src/components/FinanceCard/index.jsx
@@ -1,6 +1,6 @@
+import React, { useState } from "react";
 
 export const Total = ({ totalValue }) => {
-  // Verifica se totalValue é um número antes de chamar toFixed(2)
   const formattedTotal = typeof totalValue === 'number' ? totalValue.toFixed(2) : '0.00';
 
   return (
@@ -14,17 +14,20 @@ export const Total = ({ totalValue }) => {
   );
 };
 
-export const FinanceCard = ({ description, value }) => {
-  // Verifica se value é um número antes de chamar toFixed(2)
-  const formattedValue = typeof value === 'number' ? value.toFixed(2) : '0.00';
+export const FinanceCard = ({ description, value, type, onDelete }) => {
+  const cardClass = type === "Entrada" ? "positive" : "negative";
+  
+  const handleDeleteClick = () => {
+    onDelete(description, value, type);
+  };
 
   return (
     <>
-      <div className="card positive">
+      <div className={`card ${cardClass}`}>
         <h2 className="title three titleCard">{description}</h2>
-        <p className="title Headline pCard">Entrada</p>
-        <p className="title four pCard pCardValue">R$ {formattedValue}</p>
-        <button className="button delete btnCard">Excluir</button>
+        <p className="title Headline pCard">{type}</p>
+        <p className="title four pCard pCardValue">R$ {value.toFixed(2)}</p>
+        <button className="button delete btnCard" onClick={handleDeleteClick}>Excluir</button>
       </div>
     </>
   );
