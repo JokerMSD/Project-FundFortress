16cba2085dcaeb753e7c5fe466312442a43f2286
JokerMSD <igorpn2004@gmail.com>
2023-09-30 18:36:20 -0300

    update src/components/FinanceCardFather/index.jsx

--- src/components/FinanceCardFather/index.jsx
+++ src/components/FinanceCardFather/index.jsx
@@ -1,44 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { Total } from "../FinanceCard";
 import { FinanceCard } from "../FinanceCard";
+import { FinanceForm } from "../FinanceForm";
 
 export const FinanceApp = () => {
-  const finances = [
-    { description: "Salário - Mês Dezembro", value: 6660 },
-    { description: "Salário - Mês Dezembro", value: 6660 },
-    { description: "Salário - Mês Dezembro", value: 6660 },
-    { description: "Salário - Mês Dezembro", value: 6660 },
-    { description: "Salário - Mês Dezembro", value: 6660 },
-    { description: "Salário - Mês Dezembro", value: 6660 },
-    { description: "Salário - Mês Dezembro", value: 6660 },
-    { description: "Salário - Mês Dezembro", value: 6660 },
-    { description: "Salário - Mês Dezembro", value: 6660 },
-    { description: "Salário - Mês Dezembro", value: 6660 },
-    { description: "Salário - Mês Dezembro", value: 6660 },
-    { description: "Salário - Mês Dezembro", value: 6660 },
-    { description: "Salário - Mês Dezembro", value: 6660 },
-    { description: "Salário - Mês Dezembro", value: 6660 },
-    { description: "Outra Receita", value: 1500 },
-  ];
-
-  const totalValue = finances.reduce(
-    (total, finance) => total + finance.value,
-    0
-  );
+    const [finances, setFinances] = useState([]);
+    const [total, setTotal] = useState(0);
+  
+    const handleDelete = (description, value, type) => {
+      const updatedFinances = finances.filter((finance) => finance.description !== description);
+  
+      setFinances(updatedFinances);
+  
+      if (type === "Entrada") {
+        setTotal(total - value);
+      } else {
+        setTotal(total + value);
+      }
+    };
 
   return (
-    <>
-      <Total totalValue={totalValue} />
-      <div className="financeLi">
-      <h2 className="title two financeListTitle">Resumo Financeiro</h2>
+    <div>
+      <FinanceForm
+        finances={finances}
+        setFinances={setFinances}
+        total={total}
+        setTotal={setTotal}
+      />
+        <Total totalValue={total} />
+        <h2 className="title two financeListTitle">Resumo Financeiro</h2>
       {finances.map((finance, index) => (
         <FinanceCard
           key={index}
           description={finance.description}
           value={finance.value}
+          type={finance.valueType}
+          onDelete={handleDelete}
         />
-        ))}
-        </div>
-    </>
+      ))}
+    </div>
   );
 };
