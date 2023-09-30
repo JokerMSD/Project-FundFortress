dbca09bc36fba3d65a3659638b08f486f3a3ffb1
JokerMSD <igorpn2004@gmail.com>
2023-09-29 23:38:32 -0300

    update src/components/FinanceList/index.jsx

--- src/components/FinanceList/index.jsx
+++ src/components/FinanceList/index.jsx
@@ -1,23 +1,22 @@
-import { FinanceCard } from "../FinanceCard"
-
+import { FinanceCard } from "../FinanceCard";
 
 export const FinanceList = () => {
-
-    return (
-       <>
-       <h2 className="title two">Resumo Financeiro</h2>
-       <ul>
-        <li>
-        <FinanceCard />
-        </li>
-        <li>
-        <FinanceCard />
-        </li>
-        <li>
-        <FinanceCard />
-        </li>
-       </ul>
-      </>
-  )
-
-}
+  return (
+    <>
+      <section className="financeList">
+        <h2 className="title two">Resumo Financeiro</h2>
+        <ul className="financeUl">
+          <li className="financeLi">
+            <FinanceCard />
+          </li>
+          <li className="financeLi">
+            <FinanceCard />
+          </li>
+          <li className="financeLi">
+            <FinanceCard />
+          </li>
+        </ul>
+      </section>
+    </>
+  );
+};
