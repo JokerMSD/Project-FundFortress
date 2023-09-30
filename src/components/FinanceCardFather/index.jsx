1d6d1a07c7e3346595493a15c09204bb0d742dc5
JokerMSD <igorpn2004@gmail.com>
2023-09-30 19:49:01 -0300

    update src/components/FinanceCardFather/index.jsx

--- src/components/FinanceCardFather/index.jsx
+++ src/components/FinanceCardFather/index.jsx
@@ -4,40 +4,49 @@ import { FinanceCard } from "../FinanceCard";
 import { FinanceForm } from "../FinanceForm";
 
 export const FinanceApp = () => {
-    const [finances, setFinances] = useState([]);
-    const [total, setTotal] = useState(0);
-  
-    const handleDelete = (description, value, type) => {
-      const updatedFinances = finances.filter((finance) => finance.description !== description);
-  
-      setFinances(updatedFinances);
-  
-      if (type === "Entrada") {
-        setTotal(total - value);
-      } else {
-        setTotal(total + value);
-      }
-    };
+  const [finances, setFinances] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  const handleDelete = (description, value, type) => {
+    const updatedFinances = finances.filter(
+      (finance) => finance.description !== description
+    );
+
+    setFinances(updatedFinances);
+
+    if (type === "Entrada") {
+      setTotal(total - value);
+    } else {
+      setTotal(total + value);
+    }
+  };
 
   return (
-    <div>
+    <>
+      <section className="financeFormSection">
       <FinanceForm
         finances={finances}
         setFinances={setFinances}
         total={total}
         setTotal={setTotal}
       />
-        <Total totalValue={total} />
+
+      <Total totalValue={total} />
+      </section>
+    <section className="financeSection">
         <h2 className="title two financeListTitle">Resumo Financeiro</h2>
-      {finances.map((finance, index) => (
-        <FinanceCard
-          key={index}
-          description={finance.description}
-          value={finance.value}
-          type={finance.valueType}
-          onDelete={handleDelete}
-        />
-      ))}
-    </div>
+      <section className="cardSection">
+        {finances.map((finance, index) => (
+          <FinanceCard
+            key={index}
+            description={finance.description}
+            value={finance.value}
+            type={finance.valueType}
+            onDelete={handleDelete}
+          />
+        ))}
+      </section>
+    </section>
+    </>
   );
 };
