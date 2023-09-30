5c4ba34a0bdf2e7e03f2b07d902187d24658e2b2
JokerMSD <igorpn2004@gmail.com>
2023-09-30 15:54:52 -0300

    update src/components/FinanceCard/index.jsx

--- src/components/FinanceCard/index.jsx
+++ src/components/FinanceCard/index.jsx
@@ -1,17 +1,31 @@
 
+export const Total = ({ totalValue }) => {
+  // Verifica se totalValue é um número antes de chamar toFixed(2)
+  const formattedTotal = typeof totalValue === 'number' ? totalValue.toFixed(2) : '0.00';
 
-export const FinanceCard = () => {
+  return (
+    <>
+      <section className="Totalsection">
+        <h2 className="title three totaltext">Valor total:</h2>
+        <p className="title Headline ptext">O valor se refere ao saldo</p>
+        <p className="title three totalvalue">R$ {formattedTotal}</p>
+      </section>
+    </>
+  );
+};
 
-    return (
-       <>
-       <div className="card positive">
-        <h2 className="title three titleCard">Salário - Mês Dezembro</h2>
+export const FinanceCard = ({ description, value }) => {
+  // Verifica se value é um número antes de chamar toFixed(2)
+  const formattedValue = typeof value === 'number' ? value.toFixed(2) : '0.00';
+
+  return (
+    <>
+      <div className="card positive">
+        <h2 className="title three titleCard">{description}</h2>
         <p className="title Headline pCard">Entrada</p>
-        <p className="title four pCard pCardValue">R$ 6.660,00</p>
+        <p className="title four pCard pCardValue">R$ {formattedValue}</p>
         <button className="button delete btnCard">Excluir</button>
-       </div>
-       
-      </>
-  )
-
-}
+      </div>
+    </>
+  );
+};
