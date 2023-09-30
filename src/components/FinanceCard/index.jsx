9581ec20bde91d20f26cd30e2b1a2c7ec2b4032d
JokerMSD <igorpn2004@gmail.com>
2023-09-30 00:22:28 -0300

    update src/components/FinanceCard/index.jsx

--- src/components/FinanceCard/index.jsx
+++ src/components/FinanceCard/index.jsx
@@ -4,10 +4,10 @@ export const FinanceCard = () => {
 
     return (
        <>
-       <div className="card">
+       <div className="card positive">
         <h2 className="title three titleCard">Salário - Mês Dezembro</h2>
         <p className="title Headline pCard">Entrada</p>
-        <p className="title four pCard">R$ 6.660,00</p>
+        <p className="title four pCard pCardValue">R$ 6.660,00</p>
         <button className="button delete btnCard">Excluir</button>
        </div>
        
